0cefc451e6726bdec40d9f0bd8cf0d6700eb3714
Dossym <dosymd@gmail.com>
2024-06-27 19:15:16 +0500

    git commit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Декларирование создание через const les
 // Интерпритуриемый сверху вниз читается
 
+// const { text } = require("express")
+
 // Движок
 
 // Initialising - создаю переменную и сразу присваеваем это только в const обязательно
@@ -109,4 +111,173 @@
 
 //console.log(`Мое имя - ${name}, и изучаю фортенд`)
 
+// LESSONS 2 27.06.24
 
+// ALERT
+// alert(`ошибка сервера`)  // На движке V8 на браузерах
+
+//PROMPT
+// const age = prompt(`Сколько тебе лет?`) //Все что пишется возвращается как строка
+// console.log(age)
+// console.log(+age) // + тут конвертирует в строку
+
+// CONFIRM
+// const answer = confirm('Согласны ли вы с условием пользователся?') // Блокирует процесс выполнения кода
+// console.log(answer)
+
+// TRUE of FALSE
+// 5 > 5 => false
+// 6 <= 6 => true
+
+// IF ELSE STATEMENT
+// const statement = 5 === 5
+// if (statement) {// Условия if(ture)
+//     console.log('Ответ верный!')
+// }
+// else {
+//     console.log('Ответ верный!')
+// }
+
+// Задача
+// Создать код, который будет спрашивать у пользователя 2 цифры. Если 2 цифры одинаковые в alert-у выводить "числа равны",
+// в остальном случае выводить "числа не равны"
+
+// Решение:
+// const questionNumberOne = prompt('Введите первую цифру')
+// const questionNumberTwo = prompt('Введите вторую цифру')
+
+// if (isNaN(+questionNumberOne)) {
+//     alert('Ваш первый ответ не числ! Пожалуйста попробуйте еще раз')
+// }
+// else if (isNaN(+questionNumberTwo)) {
+//     alert('Ваш второй ответ не числ! Пожалуйста попробуйте еще раз')
+// }
+// else if (+questionNumberOne === +questionNumberTwo) {
+//     alert("Числа ровны!")
+// }
+// else {
+//     alert("Числа не равны")
+// }
+
+
+// const num1 = '18'
+// console.log(+num1)
+
+// ОПЕРАТОРЫ И/ИЛИ
+// // && - И
+// // || - ИЛИ
+
+// if(true || false) { // if(true)
+//     console.log('true')
+// }
+
+// if(false || true) { // if(true)
+//     console.log('true')
+// }
+
+// if(true || true) { // if(ture)
+//     console.log('true')
+// }
+
+// if(false || false) { // if(false)
+//     console.log('false')
+// }
+
+// // И
+// // if(ture && false) { // if(false)
+// //     console.log(true&&false)
+// // }
+// // if(false && true) { // if(false)
+// //     console.log('false && true')
+// // }
+
+// if(5 > 10 || 20 > 5) { // if(false || true)
+//     console.log('5 > 10 || 20 > 5')
+// }
+// if(10 !== 5 )
+
+
+// Можно ли пить алкоголь
+// const age = +prompt('Введите ваш возраст числами:')
+// if (isNaN(+age)) {
+//     alert('Введите возраст числами! Пожалуйста попробуйте еще раз')
+// }
+// else if (age >= 21) {
+//     alert('Вам можно пить алкоголь. Поздравляем!!!')
+// }
+// else {
+//     alert('Увы, вам нельзя пить алкоголь')
+// }
+
+// Задача:
+// Придумай код который проверяет человека на то что он подросток. 13-19
+// const age = +prompt('Введите ваш возраст числами:')
+// if (isNaN(+age)) {
+//     alert('Вы подросток? Введите ваш возраст')
+// }
+// else if (age <= 12) {
+//     alert('Вы еще не подросток')
+// }
+// else if (age >= 20) {
+//     alert('Вы уже старше подростка. Поздравляем!!!')
+// }
+// else if (age >= 13 && age <= 19) {
+//     alert('Поздравляю вы подросток!')
+// }
+
+// Как прошще
+// const age = +prompt('Введите ваш возраст числами:')
+// if (isNaN(+age)) {
+//     alert('Вы подросток? Введите ваш возраст')
+// }
+// else if (age >= 13 && age <= 19) { // интервал (от чего до чего-то) -> &&
+//     alert('Поздравляю вы подросток!')
+// }
+// else {
+//     alert('Увы, вы не подросток!')
+// }
+
+//Задача 1
+// const name = prompt('Введите ваше имя:')
+// alert(`Привет, ${name}!`)
+
+//Задача 2
+// const number = prompt('Ввеите ваше число: ')
+// const numberDegree = prompt('В какую степень возвести ваше число?')
+// console.log(number ** numberDegree)
+
+// Задача 4
+// let textOne = 'my text'
+
+// if (textOne === 'some text') {
+//     textOne = 'another text'
+// }
+// else {
+//     textOne = 'some text'
+// }
+// alert(textOne)
+
+// Задача 5
+// let number = 1
+// if (number === 0) {
+//     number = 1
+// }
+// else if (number < 0) {
+//     number = 'less then zero'
+// }
+// else {
+//     number *= 10 // number = number * 10
+// }
+// console.log(number)
+
+// Задача 6
+// const number = +prompt('Введите ваше число:')
+// if (isNaN(number)) {
+//     console.log('Введите цифрами!')
+// }
+// else if (number < 5) {
+//     console.log('Ваше число 0')
+// }
+// else {
+//     console.log('Ваше число 1')
+// }
